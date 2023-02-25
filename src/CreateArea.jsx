ee61a6049464e89e2beb9d5504c397d84cabf696
Kevin-DeBruyne <mukulupadhyay073@gmail.com>
2023-02-26 00:52:25 +0530

    post working now

--- src/CreateArea.jsx
+++ src/CreateArea.jsx
@@ -1,65 +1,64 @@
 import React, { useState } from "react";
 import axios from "axios";
 function CreateArea() {
-    const [head, sethead] = useState("");
-    const [body, setbody] = useState("");
+  const [head, sethead] = useState("");
+  const [body, setbody] = useState("");
   const handleInputChange = (e) => {
-      sethead(e.target.value);
-    };
-    const handlebodyChange = (e) => {
-      setbody(e.target.value);
-  }; 
+    sethead(e.target.value);
+  };
+  const handlebodyChange = (e) => {
+    setbody(e.target.value);
+  };
   const article = {
     title: head,
-    content : body
-  }
-   const handleSubmit = (e) => {
-     e.preventDefault();
-     fetch("http://localhost:5000/articles", {
-       method: "POST",
-       headers: {
-         "Content-Type": "application/json",
-       },
-       body: JSON.stringify(article)
-     })
-       .then((response) => {
-         console.log(response);
-         return response.text();
-         // Do something with the response
-       })
-       .then((data) => {
-         console.log(data);
-       })
-       .catch((error) => {
-         console.log(error);
-         // Handle the error
-       });
-   };
-    return (
-      <div>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <input
-              type="text"
-              name="title"
-              placeholder="Title"
-              onChange={handleInputChange}
-            />
-            <br />
-            <textarea
-              placeholder="Take a note..."
-              onChange={handlebodyChange}
-            ></textarea>
-          </div>
+    content: body,
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(article);
+    await fetch("http://localhost:5000/articles", {
+      method: "POST",
+      body: JSON.stringify(article),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        return response.text();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            onChange={handleInputChange}
+          />
           <br />
-          <div style={{ width: "30%" }}>
-            <button className="btn btn-success" type="submit">
-              Create
-            </button>
-          </div>
-        </form>
-      </div>
-    );
+          <textarea
+            placeholder="Take a note..."
+            onChange={handlebodyChange}
+          ></textarea>
+        </div>
+        <br />
+        <div style={{ width: "30%" }}>
+          <button className="btn btn-success" type="submit">
+            Create
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default CreateArea;
